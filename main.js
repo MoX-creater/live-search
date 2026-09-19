2ff29052254afaa51d007dc4fc9b7678d2c0d5f1
mohit <mohitk3001@gmail.com>
2026-09-19 19:42:44 +0530

    Add searching for mails too

--- main.js
+++ main.js
@@ -10,16 +10,33 @@ const users = [
     { name: "Priya Singh", email: "priya@gmail.com" }
 ];
 
-
+//display name and email on search 
 input.addEventListener("input", ()=>{
   const val = input.value.toLowerCase();
 
   const searchresult = users.filter(user => {
-    return user.name.toLowerCase().includes(val);
+    return user.name.toLowerCase().includes(val) || user.email.toLowerCase().includes(val);
   })
 
-  resultContainer.innerText = "";
+  resultContainer.innerHTML = "";
+
+  searchresult.forEach(user => {
+    const div = document.createElement("div");
     
-  searchresult.forEach()
+    // display name 
+    const name = document.createElement("h3");
+    name.textContent = user.name;
+    
+    const mail = document.createElement("p");
+    mail.textContent = user.email;
+
+    div.appendChild(name);
+    div.appendChild(mail);
+
+    resultContainer.appendChild(div);
+
+  });
+    
+
   console.log(searchresult);
 })
